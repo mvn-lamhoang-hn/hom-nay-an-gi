626d9dfea72d09cb8a4ff92f7d1b14cb2b473fde
Lam Hoang T. VN HN <lam.hoang@monstar-lab.com>
2023-08-04 16:29:32 +0700

    demo 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $foods = [
+        [
+            'name' => 'Bún bò',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún cá',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún chả/Bánh cuốn',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún đậu',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún ngan',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún ốc',
+            'description' => '',
+        ],
+        [
+            'name' => 'Bún riêu',
+            'description' => '',
+        ],
+        [
+            'name' => 'Dê',
+            'description' => '',
+        ],
+        [
+            'name' => 'KFC',
+            'description' => '',
+        ],
+        [
+            'name' => 'Lươn',
+            'description' => '',
+        ],
+        [
+            'name' => 'Phở/cơm rang',
+            'description' => '',
+        ],
+    ];
+    $now = now();
+    $triggerTime = Carbon::create($now->year, $now->month, $now->day, 11, 0, 0);
+    if ($now->lt($triggerTime)) {
+        $todayFood = null;
+    } else {
+        $foodCache = Cache::get($now->toDateString());
+        if (!$foodCache) {
+            $lastFoodCache = Cache::get($now->copy()->subDay()->toDateString());
+            $foodCache = rand(1, count($foods));
+            if ($lastFoodCache) {
+                while ($lastFoodCache == $foodCache) {
+                    $foodCache = rand(1, count($foods));
+                }
+            }
+            Cache::forget($now->copy()->subDay()->toDateString());
+            Cache::put($now->toDateString(), $foodCache);
+            $foodCache = Cache::get($now->toDateString());
+        }
+        $todayFood = $foods[$foodCache - 1];
+    }
+    return view('welcome')->with(compact('todayFood'));
 });
