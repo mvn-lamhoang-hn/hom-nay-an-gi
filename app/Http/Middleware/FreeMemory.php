fa2a5088963435a63d2360013572cd1fcf39fc1e
Lam Hoang T. VN HN <lam.hoang@monstar-lab.com>
2024-03-17 03:10:32 +0700

    clear cache

---
+++ app/Http/Middleware/FreeMemory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\HttpFoundation\Response;
+
+class FreeMemory
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        ini_set('memory_limit', '80M');
+        $response = $next($request);
+        Artisan::call('optimize:clear');
+        return $response;
+    }
+}
